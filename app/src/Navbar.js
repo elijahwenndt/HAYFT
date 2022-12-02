ba084851d4e68986c04f2f545e189e08f2594b7d
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-12-02 12:57:16 +0000

    done with a few stories

--- app/src/Navbar.js
+++ app/src/Navbar.js
@@ -1,6 +1,6 @@
 export default function Nav(props) {
   return (
-    <div className="container my-3 ">
+    <div className="container my-3 sticky-top">
       <div
         className="row text-center align-items-center hover text-white bg-primary rounded p-2"
         onClick={() => props.handleClick(props.text)}
@@ -8,9 +8,9 @@ export default function Nav(props) {
         <h4 className="col-2 fw-bold hover">
           HAYFT
         </h4>
-        <div className="col-4 text-start hover">YourJourney</div>
-        <div className="col-3 text-end hover">Sign up</div>
-        <div className="col-3 text-start hover">Log in</div>
+        <div className="col-6 text-start hover">YourJourney</div>
+        <div className="col-2 text-end hover">Sign up</div>
+        <div className="col-2 text-start hover">Log in</div>
       </div>
     </div>
   );
