354291608202afb2f668cea83025c4149e946f3c
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-12-05 19:54:35 +0000

    proper user data get

--- app/src/components/Profile.js
+++ app/src/components/Profile.js
@@ -1,12 +1,36 @@
 import React from "react"
 import { useGlobalState } from "../context/GlobalState";
+import request from "../services/api.request";
+import { useState, useEffect } from "react";
 
 const Profile = () => {
   const [ state, dispatch ] = useGlobalState();
+  const [postData, setPostData] = useState([]);
 
+  useEffect(() => {
+    async function getData() {
+      let options = {
+        url: "posts/",
+        method: "GET",
+        params: {
+          'author__id': state.currentUser.user_id
+        }
+
+      };
+      let resp = await request(options)
+      await dispatch ({
+        postData: resp.data
+      });
+      setPostData(resp.data);
+      
+    }
+    getData();
+  }, []);
+  console.log(postData)
   return (
     <div>
       <h1>{state.currentUser.user_id}</h1>
+
     </div>
   )
 }
