bf121c0e93a864295e35d22c07b8e03f87248102
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-12-05 10:45:32 +0000

    save

--- app/src/App.js
+++ app/src/App.js
@@ -3,17 +3,29 @@ import React from "react";
 import Navbar from "./Navbar"
 import HomePage from "./Homepage";
 import Postpage from "./Postpage"
+import { GlobalProvider } from './context/GlobalState';
+
 export default function App() {
+
   const [page, setPage] = useState("Home");
+
   console.log(page)
   function handleClick(text) {
     setPage(text);
   }
   if (page === "Home") {
-    return <HomePage handleClick={handleClick}/>;
+    return (
+      <GlobalProvider>
+      <HomePage handleClick={handleClick}/>
+      </GlobalProvider>
+    );
   }
   if (page === "YourJourney") {
-    return <Postpage handleClick={handleClick}/>;
+    return (
+      <GlobalProvider>
+      <Postpage handleClick={handleClick}/>
+      </GlobalProvider>
+      );
   }
   return (
     <>
